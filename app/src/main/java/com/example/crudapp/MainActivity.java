d92a0ecdbe120571662b8869af7149f394dbc88a
annasbachtiar <anasbachtiar801@gmail.com>
2022-11-10 17:10:37 +0700

    Add Menu and Delete Feature

--- app/src/main/java/com/example/crudapp/MainActivity.java
+++ app/src/main/java/com/example/crudapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.crudapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -55,5 +58,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
         listView.setSelected(true);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final String selection = daftar[i];
+                final CharSequence[] dialogitem = {"View Data", "Update Data", "Delete Data"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Menu");
+                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int item) {
+                        switch (item) {
+                            case 0:
+                                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                                intent.putExtra("RUANGAN_KEY", selection);
+                                startActivity(intent);
+                                break;
+                            case 1:
+                                Intent intent2 = new Intent(getApplicationContext(), UpdateActivity.class);
+                                intent2.putExtra("RUANGAN_KEY", selection);
+                                startActivity(intent2);
+                                break;
+                            case 2:
+                                SQLiteDatabase db = database.getWritableDatabase();
+                                db.execSQL("Delete FROM mahasiswa where nama = '" + selection + "'");
+                                RefreshList();
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
     }
 }
